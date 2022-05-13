431465751fb3122afc94c4f5c7e0c92f290daa8d
msolimanom <marco.solimanomiranda@amaris.com>
2022-05-13 09:54:36 -0500

    init

---
+++ src/solid/03-liskov-b.ts
@@ -0,0 +1,77 @@
+
+//clases abstractas utlizadas recomendable para herencia
+export abstract class Vehicle {
+
+    // getNumberOfSeats():number {
+    //     throw Error('Method not implemented');
+    // }
+    abstract getNumberOfSeats(): number;
+
+}
+
+
+export class Tesla extends Vehicle {
+
+    constructor(private numberOfSeats: number) {
+        super();
+    }
+
+    getNumberOfSeats() {
+        return this.numberOfSeats;
+    }
+}
+
+export class Audi extends Vehicle {
+
+    constructor(private numberOfSeats: number) {
+        super();
+    }
+
+    getNumberOfSeats() {
+        return this.numberOfSeats;
+    }
+}
+
+export class Toyota extends Vehicle {
+
+    constructor(private numberOfSeats: number) {
+        super();
+    }
+
+    getNumberOfSeats() {
+        return this.numberOfSeats;
+    }
+}
+
+export class Honda extends Vehicle {
+
+    constructor(private numberOfSeats: number) {
+        super();
+    }
+    //getNumberHondaOfSeats()
+    getNumberOfSeats() {
+        return this.numberOfSeats;
+    }
+}
+
+export class Volvo extends Vehicle {
+
+    constructor(private numberOfSeats: number) {
+        super();
+    }
+    // getNumberVolvoOfSeats()
+    getNumberOfSeats() {
+        return this.numberOfSeats;
+    }
+}
+
+export class Ford extends Vehicle {
+
+    constructor(private numberOfSeats: number) {
+        super();
+    }
+    //getNumberFordOfSeats()
+    getNumberOfSeats() {
+        return this.numberOfSeats;
+    }
+}
